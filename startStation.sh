c997b6675b63c265213b406fc9459eb364685e21
Tay Hao Cheng <42330555+Chindianese@users.noreply.github.com>
2022-11-07 13:56:23 +0800

    run simul

--- startStation.sh
+++ startStation.sh
@@ -7,5 +7,5 @@ git fetch
 git reset --hard
 git pull origin master
 # git remote set-url origin https://github.com/Chindianese/PiPager.git
-sh openMidori.sh && npm start
+sh openMidori.sh & npm start
 #cd ../
