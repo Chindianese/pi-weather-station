76d92a4a206792dc65ac2fdfc56125facb311f1c
Tay Hao Cheng <42330555+Chindianese@users.noreply.github.com>
2022-11-07 15:11:22 +0800

    Update startStation.sh

--- startStation.sh
+++ startStation.sh
@@ -6,6 +6,7 @@ cd /home/hello/pi-weather-station
 git fetch
 git reset --hard
 git pull origin master
+python3 python/setup.py
 # git remote set-url origin https://github.com/Chindianese/PiPager.git
 sh openMidori.sh & npm start
 #cd ../
